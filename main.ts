63e29b2d30859d4fb00cb6fbd46d0a2558186128
lastknighcoder <2223106858@qq.com>
2022-04-20 15:37:47 +0800

    fix:bug

--- main.ts
+++ main.ts
@@ -2,17 +2,20 @@ import {  Plugin, MarkdownPostProcessorContext } from 'obsidian';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import * as antd from 'antd';
-import Babel from '@babel/standalone';
+import * as Babel from '@babel/standalone';
 
 
 export default class Antd extends Plugin {
 	async onload() {
 
+		// @ts-ignore
 		window.antd = antd;
+		// @ts-ignore
 		window.React = React;
+		// @ts-ignore
 		window.ReactDOM = ReactDOM;
 
-		this.registerMarkdownCodeBlockProcessor('antd', (source: string, el: HTMLElement, ctx: MarkdownPostProcessorContext) => {			
+		this.registerMarkdownCodeBlockProcessor('antd', (source: string, el: HTMLElement, ctx: MarkdownPostProcessorContext) => {					
 			eval(this.transpileCode(source));
 		});
 	}
